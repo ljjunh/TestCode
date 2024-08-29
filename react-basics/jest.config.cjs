32838391862a181739f991e684624b48d08add6a
limjunhee <limjunhee4575@gmail.com>
2024-08-29 18:00:39 +0900

    24/08/29

--- react-basics/jest.config.cjs
+++ react-basics/jest.config.cjs
@@ -1,4 +1,12 @@
+// module.exports = {
+//     preset: 'ts-jest',
+//     testEnvironment: 'jest-environment-jsdom',
+// }
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-}
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
